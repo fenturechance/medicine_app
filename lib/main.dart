018b8f0f3a5eb0023e97a14ffc8e8ba18fa785bb
fenturechance <fenturechance@gmail.com>
2022-02-02 17:40:57 +0800

    finish button style

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medicine_app/database/manager/record_manager.dart';
-
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('HKT線上教室'),
-      ),
+      backgroundColor: const Color(0xffa2c4ca),
       body: HomePage(),
     ));
   }
@@ -22,48 +20,46 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            child: const Text(
-              '新增',
-              style: TextStyle(fontSize: 20),
-            ),
-            onPressed: () {
-              RecordManager.instance.insert();
-            },
+    Size size = MediaQuery.of(context).size;
+    String nowDate = DateFormat('yyyy/MM/dd EEEE').format(DateTime.now());
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        width: size.width,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                nowDate,
+                style: TextStyle(color: Color(0xff27726c), fontSize: 25.0),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              generateButton(text: '早'),
+              generateButton(text: '中'),
+              generateButton(text: '晚'),
+              generateButton(text: '睡'),
+            ],
           ),
-          ElevatedButton(
-            child: const Text(
-              '查詢',
-              style: TextStyle(fontSize: 20),
-            ),
-            onPressed: () {
-              RecordManager.instance.query();
-            },
-          ),
-          ElevatedButton(
-            child: const Text(
-              '修改',
-              style: TextStyle(fontSize: 20),
-            ),
-            onPressed: () {
-              RecordManager.instance.update();
-            },
-          ),
-          ElevatedButton(
-            child: const Text(
-              '刪除',
-              style: TextStyle(fontSize: 20),
-            ),
-            onPressed: () {
-              RecordManager.instance.delete();
-            },
-          ),
-        ],
-      ),
-    );
+        ));
   }
+}
+
+Widget generateButton({String text = ''}) {
+  return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            elevation: 0,
+            primary: const Color(0xffffd6d7),
+            minimumSize: const Size.fromHeight(100)),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20, color: Color(0xffe06667)),
+        ),
+        onPressed: () {
+          RecordManager.instance.insert();
+        },
+      ));
 }
